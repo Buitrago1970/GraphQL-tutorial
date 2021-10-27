5c485efad653b1e1fe0fba4ceeb28968a22bd7eb
Sebastian <juanbuitrago2002@outlook.com>
2021-10-27 14:07:13 -0500

    custom types

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
-/* import { graphql, buildSchema } from 'graphql'; */
-const { graphql, buildSchema } = require('graphql');
+const { buildSchema } = require("graphql");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { readFileSync } = require("fs");
+const { join } = require("path");
 
-//define schema
-const schema = buildSchema(`
-    type Query {
-        hello: String
-        saludo: String
-    }
-`);
-//query and resolver
-const resolvers = {
-    hello:()=>{
-        return 'Hello World';       
-    },
-    saludo:()=>{
-        return 'Hola Mundo';
-    }
-    
-}
+const app = express();
+const port = process.env.PORT || 3000;
 
-//run query hello
-graphql(schema, '{saludo}', resolvers)
-.then((data) => {console.log(data)})
+// define schema
+const schema = buildSchema(
+  readFileSync(join(__dirname, "lib", "schema.graphql"), "utf-8")
+);
+
+//definiendo resolver
+const resolvers = require("./lib/resolver");
+
+app.use(
+  "/api",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true,
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localHost:${port}/api`);
+});
